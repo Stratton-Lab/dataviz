d5b0dd91b07a1f67303a240aaaf1797b8907fbfb
namemcguffin@rhizome <52169277+namemcguffin@users.noreply.github.com>
2022-05-27 19:15:47 -0400

    dev: simplified tsconfig.json and vite.config.ts.

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,9 @@ import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
 
 export default defineConfig({
-    define: {
-        "import.meta.vitest": "undefined",
-    },
     base: "/dataviz/",
     plugins: [solidPlugin()],
     build: {
-        target: 'esnext',
-        polyfillDynamicImport: false,
+        target: "esnext"
     },
 });
